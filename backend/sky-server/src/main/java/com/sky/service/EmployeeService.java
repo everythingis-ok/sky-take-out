f933ce4a6a6155b114c9bfe0ae783e084d49b12d
se0ra <2270239425@qq.com>
2026-07-17 19:33:01 +0800

    Fix: remove nested git repo in backend

---
+++ backend/sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.sky.service;
+
+import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import com.sky.result.PageResult;
+import org.apache.ibatis.annotations.Select;
+
+public interface EmployeeService {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    void save(EmployeeDTO employeeDTO);
+
+    /**
+     * 分页查询方法
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void StartOrStop(Integer status, Long id);
+
+    @Select("select * from employee where id = #{}")
+    Employee getById(Long id);
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     */
+    void update(EmployeeDTO employeeDTO);
+}
