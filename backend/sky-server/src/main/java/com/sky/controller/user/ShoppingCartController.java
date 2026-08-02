05144f33c352fe642bdc4f778227978fbefa958c
se0ra <2270239425@qq.com>
2026-08-02 19:16:26 +0800

    新增删除购物车中商品功能

--- backend/sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ backend/sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -47,4 +47,12 @@ public class ShoppingCartController {
         shoppingCartService.cleanShoppingCaet();
         return Result.success();
     }
+
+    @PostMapping("/sub")
+    @ApiOperation("删除购物车中的一个商品")
+    public Result sub(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("删除购物车中一个商品，商品：{}", shoppingCartDTO);
+        shoppingCartService.subShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
 }
--- backend/sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ backend/sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -19,4 +19,10 @@ public interface ShoppingCartService {
     List<ShoppingCart> showShoppingCart();
 
     void cleanShoppingCaet();
+
+    /**
+     * 删除购物车中的一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
